abe2d47d714c0220833d008ae06289368107218f
=Michael Gingras <mcg79@cornell.edu>
2019-03-26 15:15:47 -0400

    modularizing the veil classes

--- api/controllers/strategyController.js
+++ api/controllers/strategyController.js
@@ -3,71 +3,8 @@ var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
 var Strategy = mongoose.model('Strategy');
-const Veil = require('veil-js');
 
-
-class VeilInstance {
-  constructor(m,a,u){
-    this.veil = new Veil.default(m,a,u);
-  }
-}
-
-
-class VeilStrategy {
-  // fill this out.
-  constructor(v){
-    this.veil = v;
-  }
-
-  async openOrders(market, target, spread, amount){
-    // make this the most basic order
-  };
-
-  // cancels orders
-  // could make market a part of veil strategy
-  cancelOrders(market){
-    try{
-      const m = await this.veil.getMarket(market);
-      const orders = await this.veil.getUserOrders(m);
-      // prob want to check for orders first before mapping
-      orders.results.map((order) => {
-        if(order.status === 'open'){
-          this.veil.cancelOrder(order.uid);
-        }
-      })
-      return "orders canceled"
-    } catch(e) {
-      console.log("error " + e);
-      return e;
-    }
-  }
-}
-
-
-class ScalarStrategy extends VeilStrategy {
-  constructor(v){
-    super(v);
-  }
-
-  async openOrders(market, target, spread, amount){
-    const halfSpread = spread / 2;
-    const bidPrice = target - halfSpread;
-    const askPrice = target + halfSpread;
-    const m = await this.veil.getMarket(market);
-
-    try {
-      const longQuote  = await this.veil.createQuote(m, "buy", "long", amount, bidPrice);
-      const shortQuote = await this.veil.createQuote(m, "buy", "short", amount, 1-askPrice);
-      const longOrder = await this.veil.createOrder(longQuote, { postOnly: true });
-      const shortOrder = await this.veil.createOrder(shortQuote, { postOnly: true });
-      return "created successfully"
-    } catch(e) {
-      console.log(e);
-      return e;
-    }
-  }
-}
-
+const { VeilInstance, VeilStrategy } = require('../veil/veil.js');
 
 // TODO: protect these routes with a JWT or something
 exports.createStrategy = function(req, res) {
--- api/veil/veil.js
+++ api/veil/veil.js
@@ -1,13 +1,48 @@
 const Veil = require('veil-js');
-const dotenv = require('dotenv').config();
 
 class VeilInstance {
-
   constructor(m,a,u){
     this.veil = new Veil.default(m,a,u);
   }
+}
 
-  async run(market, spread, target){
+class VeilStrategy {
+  // fill this out.
+  constructor(v,m){
+    this.veil = v;
+    this.market = m;
+  }
+
+  async openOrders(market, target, spread, amount){
+    // make this the most basic order
+  };
+
+  // cancels orders
+  // could make market a part of veil strategy
+  async cancelOrders(market){
+    try{
+      const m = await this.veil.getMarket(market);
+      const orders = await this.veil.getUserOrders(m);
+      // prob want to check for orders first before mapping
+      orders.results.map((order) => {
+        if(order.status === 'open'){
+          this.veil.cancelOrder(order.uid);
+        }
+      })
+      return "orders canceled"
+    } catch(e) {
+      console.log("error " + e);
+      return e;
+    }
+  }
+}
+
+class SimpleBinaryStrategy extends VeilStrategy {
+  constructor(v,m){
+    super(v,m);
+  }
+
+  async openOrders(market, target, spread, amount){
     const halfSpread = spread / 2;
     const bidPrice = target - halfSpread;
     const askPrice = target + halfSpread;
@@ -18,16 +53,35 @@ class VeilInstance {
       const shortQuote = await this.veil.createQuote(m, "buy", "short", amount, 1-askPrice);
       const longOrder = await this.veil.createOrder(longQuote, { postOnly: true });
       const shortOrder = await this.veil.createOrder(shortQuote, { postOnly: true });
+      return "created successfully"
     } catch(e) {
       console.log(e);
-      return res.json(e);
+      return e;
     }
-  };
+  }
 }
 
-module.exports = VeilInstance;
-//
-// const m = process.env.MNEMONIC;
-// const a = process.env.ADDRESS;
-// const u = process.env.API_URL;
-// const v = new VeilInstance(m,a,u);
+
+class EMABinaryStrategy extends VeilStrategy {
+  constructor(v,m){
+    super(v,m);
+  }
+
+  async openOrders(market, target, spread, amount){
+    // pass
+  }
+}
+
+class LMBinaryStrategy extends VeilStrategy {
+  constructor(v,m){
+    super(v,m);
+  }
+}
+
+module.exports = {
+  VeilInstance: VeilInstance,
+  VeilStrategy: VeilStrategy,
+  SimpleBinaryStrategy: SimpleBinaryStrategy,
+  EMABinaryStrategy: EMABinaryStrategy,
+  LMBinaryStrategy: LMBinaryStrategy
+}
